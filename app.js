aff57375507faba688eb10c2babdb33fe74c2bee
ravi <ravi.kulkarni25@gmail.com>
2026-06-12 09:10:09 +0000

    updated v3.0

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('ITS Tech School — GCP ACE Training v2.0 (auto-deployed!)\n');
+  res.end('ITS Tech School — GCP ACE Training v3.0 (auto-deployed!)\n');
 });
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
